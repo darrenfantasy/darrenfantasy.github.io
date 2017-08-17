01d069ec4ad10874ff0d322bfca9f62bea40470c
darrenfantasy <782107743@qq.com>
2017-08-17 23:45:44 +0800

    update ins.js

--- source/album/ins.js
+++ source/album/ins.js
@@ -110,14 +110,14 @@
         var data = res.list[j].arr;
         var liTmpl = "";
         for (var i = 0, len = data.link.length; i < len; i++) {
-          var minSrc = 'http://oud3xi5hk.bkt.clouddn.com/' + data.link[i];
+          var minSrc = 'http://oud3xi5hk.bkt.clouddn.com/' + data.link[i]+'.jpg';
           var src = 'http://oud3xi5hk.bkt.clouddn.com/' + data.link[i];
           var type = data.type[i];
           var target = src + (type === 'video' ? '.mp4' : '.jpg');
-          src += '';
+          src += '.jpg';
 
           liTmpl += '<figure class="thumb" itemprop="associatedMedia" itemscope="" itemtype="http://schema.org/ImageObject">\
-                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + src + '">\
+                <a href="' + src + '" itemprop="contentUrl" data-size="1080x1080" data-type="' + type + '" data-target="' + target + '">\
                   <img class="reward-img" data-type="' + type + '" data-src="' + minSrc + '" src="/assets/empty.jpeg" itemprop="thumbnail" onload="lzld(this)">\
                 </a>\
                 <figcaption style="display:none" itemprop="caption description">' + data.text[i] + '</figcaption>\
