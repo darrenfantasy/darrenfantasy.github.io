852ae7b35953ec218b347fb1eade6fa44f6a9520
darrenfantasy <782107743@qq.com>
2017-08-18 18:36:29 +0800

    Site updated: 2017-08-18 18:36:29

--- album/ins.js
+++ album/ins.js
@@ -726,8 +726,7 @@
             return false;
           }
 
-          // Check if the element is visible
-          // https://github.com/jquery/jquery/blob/740e190223d19a114d5373758127285d14d6b71e/src/css/hiddenVisibleSelectors.js
+
           if (!elt.offsetWidth || !elt.offsetHeight) {
             return false;
           }
